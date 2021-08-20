cee3f5d5109a1a679c0cadf6ac644b06f86e486f
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-08-20 16:04:33 -0700

    after edit

--- app/src/main/java/com/example/currencyconverterapp/view/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverterapp/view/MainActivity.kt
@@ -1,32 +1,25 @@
 package com.example.currencyconverterapp.view
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.example.currencyconverterapp.R
 import com.example.currencyconverterapp.databinding.ActivityMainBinding
 import com.example.currencyconverterapp.viewModel.MainViewModel
-import kotlin.math.absoluteValue
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     private val viewModel : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        go()
-    }
-
-    private fun go() {
-        binding.Converter.setOnClickListener {
-            viewModel.getCurrency()
-        }
-        viewModel.currentCurrency.observe(this,{
-            binding.newCurrency.text = it.absoluteValue.toString()
-        })
+        binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
     }
 }
